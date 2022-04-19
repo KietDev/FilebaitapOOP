f1ede9cb3f2f9dc475288170ceb91d087586da9f
KietTran <kiettrana1b2@gmail.com>
2022-04-19 17:58:22 +0700

    bai 1

--- baitap.cpp
+++ baitap.cpp
@@ -1,1 +1,56 @@
-// File bai tap OOP tuan 3 //
+#include<iostream>
+using namespace std;
+
+class Point {
+    private:
+    int x, y;
+    public:
+    Point(int = 0; int = 0);
+    int getx();
+    int gety();
+    void setx(int x);
+    void sety(int y);
+    void Movex(int i);
+    void Movey(int i);
+    void Input();
+    void Output();
+};
+
+Point::Point(int a, int b) {
+    x = a; 
+    y = b;
+} 
+void Point::Input() {
+    cin >> x >> y;
+}
+void Point::Output() {
+    cout << "(" << x << "," << y << ")";
+}
+int Point::getx() {
+    return x;
+}
+int Point::gety() {
+    return y;
+}
+void Point::setx(int x) {
+    this->x = x;
+}
+void Point::sety(int y) {
+    this->y = y;
+}
+void Point::movex(int i) {
+    x = x + i;
+}
+void Point::movey(int i) {
+    y = y + i;
+}
+
+int main() {
+    Point p;
+    p.Input();
+    int i;
+    p.Movex(i);
+    p.Movey(i);
+    p.Output();
+    return 0;
+}
